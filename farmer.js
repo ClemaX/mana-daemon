5134911508372ce40d2c285dee87aa852d01248e
Clement Hamada <clementhamada@protonmail.com>
2022-01-12 14:38:41 +0100

    Convert to ES module

--- farmer.js
+++ farmer.js
@@ -1,19 +1,19 @@
-const Discord = require('discord.js-selfbot');
-const client = new Discord.Client();
-const fs = require('fs').promises;
+import { promises as fs } from 'fs';
+import { Client } from 'discord.js-selfbot';
 
-const channelId = '712645175670865951';
+const client = new Client();
 
-async function connect() {
-	const token = (await fs.readFile('token', 'utf8')).replace('\n', '');
+async function connect(channelId) {
+	const token = (await fs.readFile('token', 'utf8'));
+
+	client.on('ready', async () => {
+		console.debug('Client ready!');
+		const channel = client.channels.cache.get("");
+		await channel.join(channelId);
+		console.debug(`Joined ${channel.name}@${channel.guild.name}!`);
+	});
+
 	client.login(token);
 }
 
-client.on('ready', async () => {
-    console.debug('Client ready!');
-	const channel = client.channels.cache.get("");
-	await channel.join(channelId);
-	console.debug(`Joined ${channel.name}@${channel.guild.name}!`);
-});
-
-connect();
+connect('712645175670865951');
