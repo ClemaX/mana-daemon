4971eed2958cc5d3be72dd70b077ee0f6927035b
Clément Hamada <clementhamada@protonmail.com>
2022-01-16 17:58:07 +0100

    Make token file accessible only to the user

--- get_token.sh
+++ get_token.sh
@@ -7,6 +7,9 @@ ORIGIN=$(rev <<< discord.com)
 
 cp "$PROFILE/webappsstore.sqlite" webappsstore.sqlite
 
+# Token should be accessible only to the current user.
+umask 177
+
 sqlite3 webappsstore.sqlite <<EOF | tr -d $'"\x1e' > token
 .mode ascii
 SELECT value FROM webappsstore2 WHERE originKey LIKE "%$ORIGIN%" AND key = "token";
