99e54eb771a69d12f4b4de3549d0f853a4755b2f
Clément Hamada <clementhamada@protonmail.com>
2022-01-16 17:57:35 +0100

    Fix record separator in token

--- get_token.sh
+++ get_token.sh
@@ -7,7 +7,7 @@ ORIGIN=$(rev <<< discord.com)
 
 cp "$PROFILE/webappsstore.sqlite" webappsstore.sqlite
 
-sqlite3 webappsstore.sqlite <<EOF | tr -d '"' > token
+sqlite3 webappsstore.sqlite <<EOF | tr -d $'"\x1e' > token
 .mode ascii
 SELECT value FROM webappsstore2 WHERE originKey LIKE "%$ORIGIN%" AND key = "token";
 EOF
