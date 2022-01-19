307e279c7a92979e82ba89a5da86518da406d106
Clément Hamada <clementhamada@protonmail.com>
2022-01-19 17:08:32 +0100

    Use cache directory for token and current mana

--- farmer.js
+++ farmer.js
@@ -1,19 +1,62 @@
 import { promises as fs } from 'fs';
 import Discord from 'discord.js-selfbot';
 
-const client = new Discord.Client();
+const { Client, Intents } = Discord;
 
-async function connect(channelId) {
-	const token = (await fs.readFile('token', 'utf8'));
+var cacheDir;
 
-	client.on('ready', async () => {
-		console.debug('Client ready!');
+if (process.platform === 'linux') {
+	const cacheHome = process.env.XDG_CACHE_HOME || `${process.env.HOME}/.cache`;
+	cacheDir = `${cacheHome}/mana`;
+} else if (process.platform === 'darwin') {
+	const cacheHome = `${process.env.HOME}/Library/Caches`;
+	cacheDir = `${cacheHome}/fr.42lyon.chamada.mana`;
+} else {
+	console.error(`Unknown system: '${process.platform}'!`);
+	process.exit(1);
+}
+
+process.on('uncaughtException', function (err) {
+	console.log('Caught exception: ', err);
+});
+
+const client = new Client({
+	intents: [Intents.FLAGS.DIRECT_MESSAGES],
+	partials: ['MESSAGE', 'CHANNEL'],
+});
+
+function storeMana(manaMessage) {
+	if (manaMessage.channel.type === 'dm' && manaMessage.author.username === 'Neko') {
+		for (const embed of manaMessage.embeds) {
+			const total = embed.fields.find((field) => field.name === 'Total');
+			if (total !== undefined) {
+				console.debug('Total:', total.value);
+				fs.writeFile(`${cacheDir}/total`, total.value, { encoding: 'utf-8', mode: 0o600 });
+			}
+		}
+	}
+}
+
+async function farm(channelId, manaChannelId) {
+	const token = (await fs.readFile(`${cacheDir}/token`, 'ascii'));
+
+	client.on('message', storeMana);
+
+	client.once('ready', async () => {
+		console.debug('Ready!');
+
 		const channel = await client.channels.fetch(channelId);
-		await channel.join();
-		console.debug(`Joined ${channel.name}@${channel.guild.name}!`);
+
+		console.debug(`Joining ${channel.name}@${channel.guild.name}...`);
+		channel.join();
+
+		const manaChannel = await client.channels.fetch(manaChannelId);
+
+		manaChannel.send('!mana');
+		setInterval(() => manaChannel.send('!mana'), 30 * 60 * 1000);
 	});
 
 	client.login(token);
 }
 
-connect('712645175670865951');
+farm('712645175670865951', '710872410198376478');
--- get_token.sh
+++ get_token.sh
@@ -1,18 +1,44 @@
 #!/usr/bin/env bash
 
+# Abort on errors.
+set -euo pipefail
+
+# Get cache directory.
+UNAME=$(uname -s)
+case "$UNAME" in
+	Linux*)		CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/mana";;
+	Darwin*)	CACHE_DIR="$HOME/Library/Caches/fr.42lyon.chamada.mana";;
+	*)			echo "Unknown system: '$UNAME'!" >&2 && exit 1;;
+esac
+
+# Script directory.
 PARENT_DIR=$(dirname "$0")
 
-PROFILE=$($PARENT_DIR/select_profile.sh)
+# Firefox profile directory.
+PROFILE=$("$PARENT_DIR/select_profile.sh")
+# Origin to search for in database.
 ORIGIN=$(rev <<< discord.com)
 
-cp "$PROFILE/webappsstore.sqlite" webappsstore.sqlite
+# Temporary database copy location.
+DB_TMP="$CACHE_DIR/webappsstore.sqlite"
 
-# Token should be accessible only to the current user.
+# Token destination.
+TOKEN="$CACHE_DIR/token"
+
+# Create cache directory.
+mkdir -p "$CACHE_DIR"
+
+# Files should be accessible only to the current user.
 umask 177
 
-sqlite3 webappsstore.sqlite <<EOF | tr -d $'"\x1e' > token
+# Copy database.
+cp "$PROFILE/webappsstore.sqlite" "$DB_TMP"
+
+# Extract token.
+sqlite3 "$DB_TMP" <<EOF | tr -d $'"\x1e' > "$TOKEN"
 .mode ascii
 SELECT value FROM webappsstore2 WHERE originKey LIKE "%$ORIGIN%" AND key = "token";
 EOF
 
-rm webappsstore.sqlite*
+# Remove temporary files.
+rm "$DB_TMP"*
